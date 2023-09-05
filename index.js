9ee77530833afb76dfae2114e680d2c807e21e50
Tabish Naqvi <tabish.naqvi2003@gmail.com>
2023-09-05 19:49:31 +0530

    auth

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
+require("dotenv").config();
 const express = require('express');
 const app = express();
 const cors = require("cors");
+const connectToMongoDB = require('./db/db');
+const userRouter = require('./routes/userRoutes');
 
 const port = 8181 || process.env.PORT;
 
-app.use("cors");
+app.use(cors());
 app.use(express.json());
-app.use("/api/v1/story");
-app.use("/api/v1/user");
-app.use("/api/v1/chapter");
-app.use("/api/v1/comment");
+// app.use("/api/v1/story");
+app.use("/api/v1/user", userRouter);
+// app.use("/api/v1/chapter");
+// app.use("/api/v1/comment");
 
-app.listen(port, () => {
-    console.log(`Server Alive On http:localhost:${port}`);
+app.listen(port, async() => {
+    await connectToMongoDB(process.env.MONGO_URI);
+    console.log(`Server Alive On http://localhost:${port}`);
 });
