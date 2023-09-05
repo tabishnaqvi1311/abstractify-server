876c0353cde4e1b53510ce537ea075fd3db5e7d0
Tabish Naqvi <tabish.naqvi2003@gmail.com>
2023-09-05 19:48:46 +0530

    auth done

--- controller/userController.js
+++ controller/userController.js
@@ -6,6 +6,13 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 //TODO: Seperate mail sender
 const nodemailer = require("nodemailer");
+const transporter = nodemailer.createTransport({
+    service: "Outlook",
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD
+    }
+});
 //TODO: break down into functions, try middleware
 //TODO: use jwt in login ans signup
 const signupUser = async (req, res) => {
@@ -17,7 +24,7 @@ const signupUser = async (req, res) => {
 
     if (!validator.isStrongPassword(password)) return res.status(400).json("weak password");
 
-    if(username.length < 5) return res.status(400).json("username < 5 char!");
+    if (username.length < 5) return res.status(400).json("username < 5 char!");
 
     const doesEmailAlreadyExist = await User.findOne({ email });
 
@@ -33,7 +40,9 @@ const signupUser = async (req, res) => {
             password: hash,
         });
 
-        return res.status(201).json(newUser);
+        const token = jwt.sign({ userId: newUser._id }, process.env.SECRET, { expiresIn: "2d" });
+
+        return res.status(201).json({ newUser, token });
     } catch (error) {
         return res.status(500).json("internal server error");
     }
@@ -41,53 +50,85 @@ const signupUser = async (req, res) => {
 
 
 
-const loginUser = async(req, res) => {
-    const {username, password} = req.body;
+const loginUser = async (req, res) => {
+    const { username, password } = req.body;
 
-    if(!username || !password) return res.status(400).json("fields cannot be empty");
+    if (!username || !password) return res.status(400).json("fields cannot be empty");
 
-    const doesUsernameExist = await User.findOne({username});
-    if(!doesUsernameExist) return res.status(404).json("user does not exist");
+    const doesUsernameExist = await User.findOne({ username });
+    if (!doesUsernameExist) return res.status(404).json("user does not exist");
 
     const doPasswordsMatch = await bcrypt.compare(password, doesUsernameExist.password);
-    if(!doPasswordsMatch) return res.status(400).json("incorrect password");
+    if (!doPasswordsMatch) return res.status(400).json("incorrect password");
 
-    return res.status(200).json("user logged in");
+    const token = jwt.sign({ userId: doesUsernameExist._id }, process.env.SECRET, { expiresIn: "2d" });
+
+    return res.status(200).json({ token });
 }
 
 
 
 
-const forgotPassword = async(req, res) => {
-    const {email} = req.body;
+const forgotPassword = async (req, res) => {
+    const { email } = req.body;
 
-    const doesEmailAlreadyExist = await User.findOne({email});
-    if(!doesEmailAlreadyExist) return res.status(404).json("email not found");
-    
+    const doesEmailAlreadyExist = await User.findOne({ email });
+    if (!doesEmailAlreadyExist) return res.status(404).json("email not found");
+
     const payload = {
         email: email,
         id: doesEmailAlreadyExist._id
     }
-    const token = jwt.sign(payload, process.env.SECRET, {expiresIn: "15min"});
+    const token = jwt.sign(payload, process.env.SECRET, { expiresIn: "15min" });
 
-    const link = "link goes here with token and id";
-
+    const link = `http://localhost:3000/reset-password/${payload.id}/${token}`;
 
     const mailOptions = {
         from: process.env.EMAIL,
         to: email,
         subject: "Reset Your Account Password",
-        text: link
+        text: `We've received a request to reset the password for your Abstractify account. If you didn't make this request, you can safely ignore this email:  Your password will remain unchanged.
+
+        To reset your password, please follow the link below:
+        
+        ${link}
+        
+        This link will expire in 15 minutes for security reasons, so make sure to use it promptly.
+        
+        If you're having trouble with the link or need further assistance, please don't hesitate to contact our support team at [Support Email Address].
+        
+        To ensure you receive future emails from us, please add ABSTRACTIFY to your email's safe sender list or address book.
+        
+        Thank you for being a valued member of Abstractify. We're here to help if you have any questions or concerns.`
     }
 
+
     transporter.sendMail(mailOptions, (error, info) => {
-        if(error) return res.status(500).json({"failed to send email": error.message});
+        if (error) return res.status(500).json({ "failed to send email": error.message });
         return res.status(200).json("email sent");
     })
 }
+//TODO check if password is really being updated or not
+const resetPassword = async (req, res) => {
+    const { _id } = req.params;
+    const { password, repeatPassword } = req.body;
 
-const resetPassword = async(req, res) => {
+    if (password !== repeatPassword) return res.status(400).json({ error: "passwords must match!" });
 
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+
+    try {
+        const updatedUser = await User.findOneAndUpdate(
+            { _id },
+            { password: hash },
+        )
+        return res.status(201).json("User updated");
+    } catch (error) {
+        console.log(error.message);
+        return res.status(500).json("Internal server error");
+    }
 }
 
-module.exports = { signupUser, loginUser, forgotPassword };
+
+module.exports = { signupUser, loginUser, forgotPassword, resetPassword };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,19 @@
 const express = require("express");
-const { forgotPassword, loginUser, signupUser } = require("../controller/userController");
+const { signupUser, loginUser, forgotPassword, resetPassword } = require("../controller/userController");
+const validateResetLink = require("../middleware/validateResetLink");
+const User = require("../models/User");
 const router = express.Router();
 
+//get all users
+router.get("/getUsers", async (req, res) => {
+    try {
+        const users = await User.find({});
+        return res.status(200).json({ users });
+    } catch (error) {
+        console.log(error.message);
+        return res.status(500).json({error: error.message});
+    }
+});
 //login route
 router.post("/login", loginUser);
 //signup route
@@ -9,12 +21,9 @@ router.post("/signup", signupUser);
 //forgot password route
 router.post("/forgotPassword", forgotPassword);
 //reset password route
-router.get();
-//also reset password router
-router.post();
-//get all users
-router.get();
+//TODO: TEST THE MIDDLEWARE ONCE THE CLIENT IS IMPLEMENTED
+router.post("/resetPassword/:id/:token", resetPassword);
 //get one user
-router.get();
+// router.get();
 
 module.exports = router;
