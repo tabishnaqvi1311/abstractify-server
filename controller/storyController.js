83dee0eb2a89528ccff33a8d26affa7ead13146c
Tabish Naqvi <tabish.naqvi2003@gmail.com>
2023-09-11 22:43:33 +0530

    created post story route

--- controller/storyController.js
+++ controller/storyController.js
@@ -2,40 +2,61 @@ const Chapter = require("../models/Chapter");
 const Story = require("../models/Story");
 const User = require("../models/User");
 
-const getAllStories = async(req, res) => {
+const getAllStories = async (req, res) => {
     //TODO: add pagination
-    const stories = await Story.find({limit: 10});
+    const stories = await Story.find({ limit: 10 });
     return res.status(200).json(stories);
 }
 
-const getStory = async(req, res) => {
-    const {id} = req.body
-    const story = await Story.findOne({id: id});
-    if(!story) return res.status(404).json("story not found");
+const getStory = async (req, res) => {
+    const { id } = req.params
+    const story = await Story.findOne({ id: id });
+    if (!story) return res.status(404).json("story not found");
 
     return res.status(200).json(story);
 }
 
-const postStory = async(req, res) => {
-    const {title, genre, initialChapterTitle, initialChapterContent} = req.body;
-    //get user id
-    const id = 4; //just an example
-
-    const author = User.findById(id);
-    if(!author) return res.status(404).json("user not found with that id");
-
-    //experimental code, proceed with caution
-    //we need to create a chapter and link it to a story
+const postStory = async (req, res) => {
+    const { title, author, genre, initialChapterTitle, initialChapterContent } = req.body;
+    //validate user
+    //test controller
+    if (!title || author || genre || content) return res.status(400).json("Fill in the fields");
 
 
+    const authorId = await User.findById(author);
+    if (!authorId) return res.status(404).json("No user with that id found!");
+
+    try {
+        const story = new Story({
+            title,
+            author: authorId._id,
+            genre
+        });
+
+        await story.save();
+
+        const initialChapter = new Chapter({
+            title: initialChapterTitle,
+            content: initialChapterContent,
+            story: story._id,
+            author: authorId._id
+        });
+
+        await initialChapter.save();
+
+        return res.status(201).json({ story: newStory, chapter: initialChapter });
+    } catch (error) {
+        console.log(error.message);
+        return res.status(500).json({error: error.message, TypeError: "Internal Server Error"});
+    }
 }
 
-const updateStory = async(req, res) => {
+const updateStory = async (req, res) => {
     //update title, private/public
 }
 
-const deleteStory = async(req, res) => {
+const deleteStory = async (req, res) => {
     //delete a chapter and all related chapters as well
 }
 
-module.exports = {getAllStories, getStory};
+module.exports = { getAllStories, getStory };
